4eab9ef88851a5312897fb2af151458a65658ba3
unknown <istiaq.me@gmail.com>
2022-10-21 03:23:43 +0600

    API Authentication Middleware Done

--- app/Http/Middleware/APIAuthentication.php
+++ app/Http/Middleware/APIAuthentication.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use App\Models\App;
+use App\Services\CryptoService;
 
 class APIAuthentication
 {
@@ -16,6 +18,58 @@ class APIAuthentication
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        // get http referer
+        $requestFromApp = $request->headers->get('referer');
+        if(config('app.env') == 'local'){
+            $requestFromApp = 'Localhost'; // This is pre seeded data
+        }
+        $apiKey = $request->headers->get('auth-key');
+        if(!$requestFromApp){
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'Invalid App.'
+                ], 
+            400);
+        }
+        // check database has app information
+        $app = App::where('base_url', $requestFromApp)->where('api_key', (new CryptoService())->encrypt($apiKey))->first();
+
+        if(!$app){
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'Login Failed.'
+                ], 
+            401);
+        }
+
+        if(!$app->binded_ips){
+            // ip bind needs not to be checked, add app info to request for future use and return next
+            define('APP_ID', $app->id);
+            return $next($request);
+        }
+
+        // check for binded ips
+        if(count($app->binded_ips) == 0){
+            // ip bind needs not to be checked, add app info to request for future use and return next
+            define('APP_ID', $app->id);
+            return $next($request);
+        }
+        
+        // as binded ips are in array, check the current ip is in the list or not
+        if(in_array($request->ip(), $app->binded_ips)){
+            define('APP_ID', $app->id);
+            return $next($request);
+        }
+        else{
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'Login Failed.'
+                ], 
+            422);
+        }
+        
     }
 }
